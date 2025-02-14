d7cd790c01bd302e430eec77751bdc29e4f4c8cf
SM.dev <molisanti13@gmail.com>
2025-02-14 12:36:35 -0300

    responsive

--- notas.md
+++ notas.md
@@ -6,4 +6,32 @@
 
  hacer el hook del envio de formularios 
 
+
+import { BsMicFill } from "react-icons/bs";  AJUSTES
+
+
+import { CiLogout } from "react-icons/ci";  logout
+
+
+import { RiSearchEyeLine } from "react-icons/ri"; LUPA
+
+import { IoMdContacts } from "react-icons/io"; contactos 
+
+
+
+import { BsPaperclip } from "react-icons/bs";  CLIP
+
+import { BsMicFill } from "react-icons/bs";    MIC
+
+
+
+
+
+
+
+
+
+
+
+
  
--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,12 +1,13 @@
  import React, { useEffect, useState } from 'react'
 import ContactsScreen from './ContactsScreen'
+import { CiLogout } from "react-icons/ci";
 
 import { ChatScreen } from './ChatScreen'
 import '../styles/homeScreen.styles.css'
 
 const HomeScreen = () => {
   
-  
+  const [contacstComponent,setContactsComponent]=useState(false)
   const [contactSelect,setContactSelect]=useState(null)
  
 
@@ -18,11 +19,13 @@ const HomeScreen = () => {
     <div className='home-container'>
 
       <section className='section-container'>
-        <nav className='nav-container'> soy el nav</nav>
+        <nav className='nav-container'> <CiLogout/></nav>
         <aside className='aside-container_contacts'>
           <ContactsScreen onSelectContact={setContactSelect} className='contactos'/>
         </aside>
         <aside className='aside-container_chat'>
+          <button onClick={()=>setContactsComponent(!contacstComponent)} className='contact-button-open'>M</button>
+          { contacstComponent && <ContactsScreen onSelectContact={setContactSelect} className='contactos '/>}
           {contactSelect?<ChatScreen onSelectContact={contactSelect} className='chat'/>:null}  
         </aside>
       </section>
